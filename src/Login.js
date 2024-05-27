7b3ceb5af0683857e212fc31cff4f07d80c79533
Heistoo <46348338+Heistoo@users.noreply.github.com>
2024-05-27 11:40:21 -0300

    refactor code

--- src/Login.js
+++ src/Login.js
@@ -1,3 +1,4 @@
+// Library imports
 import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, TextInput, Alert, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -6,11 +7,80 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import CheckBox from 'expo-checkbox';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+
+// Styles import
 import styles from './index.js';
 
+// Image import
 import FB from './assets/facebookicon.svg';
 import Social2 from './assets/social2.svg';
 import Tinka from './assets/tinka-logo.svg';
+
+
+const images = {
+    'login': require('./assets/login-screen.jpg')
+}
+
+const handleLinkPress = (screenName, navigation) => {
+    navigation.navigate(screenName);
+};
+
+const fetchUserId = async (email, senha) => {
+    try {
+        const response = await axios.post(`${process.env.EXPO_PUBLIC_API_URL}/get_user_id`, { email, senha });
+        if (response.status === 200) {
+            return response.data.usuario_id;
+        } else {
+            Alert.alert('Erro ao obter ID do usuário', response.data.error || 'Algo deu errado.');
+            return null;
+        }
+    } catch (error) {
+        Alert.alert('Erro ao obter ID do usuário', error.message || 'Algo deu errado.');
+        return null;
+    }
+};
+
+const signInWithEmail = async (email, senha, setLoading, navigation) => {
+    if (email === '' || senha === '') {
+        Alert.alert('Preencha todos os campos!');
+        return;
+    }
+    if (!email.includes('@')) {
+        Alert.alert('Email inválido.');
+        return;
+    }
+    setLoading(true);
+
+    const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email,
+            senha: senha,
+        }),
+    });
+
+    try {
+        //busca o id do usuário após o login bem-sucedido
+        const usuarioId = await fetchUserId(email, senha);
+
+        if (usuarioId) {
+            //guarda o id do usuário no AsyncStorage para uso futuro
+            await AsyncStorage.setItem('usuario_id', usuarioId.toString());
+            Alert.alert('Login bem-sucedido!');
+            navigation.navigate('Jogo');
+        }
+    } catch (error) {
+        console.error('Erro ao realizar login: usuarioId não é um inteiro', error);
+        Alert.alert('Erro ao realizar login: usuarioId não é um inteiro', error.message);
+    }
+    finally{
+        setLoading(false);
+    }
+   
+};
 
 const Login = () => {
     const navigation = useNavigation();
@@ -19,73 +89,11 @@ const Login = () => {
     const [senha, setSenha] = useState('');
     const [loading, setLoading] = useState(false);
     const [showPassword, setShowPassword] = useState(false); 
+
     const toggleShowPassword = () => { 
         setShowPassword(!showPassword); 
     };
-
-    const images = {
-        'login': require('./assets/login-screen.jpg')
-    }
-
-    const handleLinkPress = (screenName) => {
-        navigation.navigate(screenName);
-    };
-
-    const fetchUserId = async (email, senha) => {
-        try {
-            const response = await axios.post(`${process.env.EXPO_PUBLIC_API_URL}/get_user_id`, { email, senha });
-            if (response.status === 200) {
-                return response.data.usuario_id;
-            } else {
-                Alert.alert('Erro ao obter ID do usuário', response.data.error || 'Algo deu errado.');
-                return null;
-            }
-        } catch (error) {
-            Alert.alert('Erro ao obter ID do usuário', error.message || 'Algo deu errado.');
-            return null;
-        }
-    };
-
-    const signInWithEmail = async () => {
-        if (email === '' || senha === '') {
-            Alert.alert('Preencha todos os campos!');
-            return;
-        }
-        if (!email.includes('@')) {
-            Alert.alert('Email inválido.');
-            return;
-        }
-        setLoading(true);
-
-        const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email,
-                senha: senha,
-            }),
-        });
-
-        try {
-            //busca o id do usuário após o login bem-sucedido
-            const usuarioId = await fetchUserId(email, senha);
-
-            if (usuarioId) {
-                //guarda o id do usuário no AsyncStorage para uso futuro
-                await AsyncStorage.setItem('usuario_id', usuarioId.toString());
-                Alert.alert('Login bem-sucedido!');
-                navigation.navigate('Jogo');
-            }
-        } catch (error) {
-            console.error('Erro ao realizar login: usuarioId não é um inteiro', error);
-            Alert.alert('Erro ao realizar login: usuarioId não é um inteiro', error.message);
-        }
-
-        setLoading(false);
-    };
-
+    
     return (
         <SafeAreaView style={styles.container2}>
             <ImageBackground source={images['login']} style={styles.containerbg}>
@@ -99,14 +107,14 @@ const Login = () => {
                         <TextInput
                             style={[styles.input2, styles.roundedInput]}
                             value={email}
-                            onChangeText={(text) => setEmail(text)}
+                            onChangeText={setEmail}
                         />
                         <Text style={styles.placeholder2}>Senha</Text>
                         <TextInput
                             style={[styles.input2, styles.roundedInput]}
                             secureTextEntry={!showPassword}
                             value={senha}
-                            onChangeText={(text) => setSenha(text)}
+                            onChangeText={setSenha}
                         />
                         <MaterialCommunityIcons 
                             name={showPassword ? 'eye-off' : 'eye'} 
@@ -125,17 +133,17 @@ const Login = () => {
                             <Text>Lembrar de mim</Text>
                         </View>
                         <View style={styles.buttoncontainer}>
-                            <TouchableOpacity onPress={signInWithEmail}>
+                            <TouchableOpacity onPress={() => signInWithEmail(email, senha, setLoading, navigation)}>
                                 <Text style={styles.text}>Login</Text>
                             </TouchableOpacity>
                         </View>
-                        <TouchableOpacity onPress={() => handleLinkPress('App')}>
+                        <TouchableOpacity onPress={() => handleLinkPress('App', navigation)}>
                             <Text style={{color: '#4D73FA'}}>Registrar-se</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => handleLinkPress('Jogo')}>
+                        <TouchableOpacity onPress={() => handleLinkPress('Jogo', navigation)}>
                             <Text style={{color: '#4D73FA'}}>TelaJogo</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => handleLinkPress('Inicial')}>
+                        <TouchableOpacity onPress={() => handleLinkPress('Inicial', navigation)}>
                             <Text style={{color: '#4D73FA'}}>Escolhe Inicial</Text>
                         </TouchableOpacity>
                         <View style={styles.containerIcon}>
